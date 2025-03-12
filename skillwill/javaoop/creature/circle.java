93c78e16d78b513d63daa3f3d945240b228ee80f
ShoreG <109215780+ShoreG@users.noreply.github.com>
2025-03-12 19:32:09 +0400

    cv lilelebi

---
+++ skillwill/javaoop/creature/circle.java
@@ -0,0 +1,23 @@
+package skillwill.javaoop.creature;
+
+public class circle {
+    private double radius;
+
+    public  circle(double radius){
+        this.radius = radius * 2;
+        System.out.println("'helloo world");
+
+    }
+
+    public void Circle(double radius) {
+
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+     }
+
+}
---
+++ skillwill/javaoop/runners/main.java
@@ -0,0 +1,23 @@
+package skillwill.javaoop.runners;
+
+import skillwill.javaoop.creature.circle;
+
+public class main {
+
+//    Circle circle2 = new Circle(4.7);
+//    Circle circle3 = new Circle(2.7);
+    public static void main(String[] args){
+        // Print the radii of the circles
+        circle circle1 = new circle(3.5);
+        System.out.println("Circle 1 radius: " + circle1.getRadius());
+//        System.out.println("Circle 2 radius: " + circle2.getRadius());
+//        System.out.println("Circle 3 radius: " + circle3.getRadius());
+
+    }
+
+
+}
+
+public class name {
+
+}
